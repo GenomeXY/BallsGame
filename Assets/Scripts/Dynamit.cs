00edcdd3523258f61331c3759fea6fa0a062d4fc
Illia Emelianenko <ie.bntu@gmail.com>
2024-05-01 17:27:10 +0300

    Создал эффект от динамита (взаимодействие с Active и Passive Items)
    Дополнил корутину AffectProcess в классе Dynamit кодом для применения силы ко всем объектам с Rigidbody в радиусе действия динамита (для их разброса), а для PassiveItem вызовом методов OnAffect.

--- Assets/Scripts/Dynamit.cs
+++ Assets/Scripts/Dynamit.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Dynamit : ActiveItem
 {
     [Header("Dynamit")]
-    [SerializeField] private float _affectRadius = 1.5f; //радиус действия
-    [SerializeField] private float _forceValue = 1000f; //сила действия
+    [SerializeField] private float _affectRadius = 2.5f; //радиус действия
+    [SerializeField] private float _forceValue = 500f; //сила действия
 
     [SerializeField] private GameObject _affectArea; 
     [SerializeField] private GameObject _effectPrefab;
@@ -28,6 +28,25 @@ public class Dynamit : ActiveItem
         _affectArea.SetActive(true); //включает круг-радуис действия, чтобы мы видели зону поражения
         _animator.enabled = true;
         yield return new WaitForSeconds(1f);
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, _affectRadius); // возвращает массив всех коллайдеров, которые пересекла сфера определенного радиуса
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            // Применяем силу ко всем Rigidbody в радиусе
+            Rigidbody rigidbody = colliders[i].attachedRigidbody;
+            if (rigidbody)
+            {
+                Vector3 fromTo = (rigidbody.transform.position - transform.position).normalized;
+                rigidbody.AddForce(fromTo * _forceValue + Vector3.up * _forceValue * 0.5f); //применяем силу от динамита + ещё немного вверх
+            }
+            // Производим эффект на каждом PassiveItem
+            PassiveItem passiveItem = colliders[i].GetComponentInParent<PassiveItem>();
+            if (passiveItem)
+            {
+                passiveItem.OnAffect();
+            }
+        }
+
         Instantiate(_effectPrefab, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
